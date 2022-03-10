9290605a40fc36d699f866e132c8e3d912c1668a
Ravi3599 <ravikantdukre2018@gmail.com>
2022-03-10 16:41:03 +0530

    ADDED: address book home and form pages with routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './MyComponents/dashboard/dashboard.component';
+import { FormComponent } from './MyComponents/form/form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'/' ,redirectTo:'dashboard', pathMatch:'full'},
+  {path:"dashboard", component:DashboardComponent},
+  {path:"form",component:FormComponent},
+  {path:"**", component:DashboardComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './MyComponents/dashboard/dashboard.component';
+import { FormComponent } from './MyComponents/form/form.component';
+import { HeaderComponent } from './MyComponents/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    DashboardComponent,
+    FormComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
